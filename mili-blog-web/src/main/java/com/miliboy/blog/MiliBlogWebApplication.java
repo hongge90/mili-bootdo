3f362f390dc38888935bddee6dfa28ca9a7daea7
hongge90 <1648089344@qq.com>
2020-10-17 11:37:29 +0800

    博客项目创建

---
+++ mili-blog-web/src/main/java/com/miliboy/blog/MiliBlogWebApplication.java
@@ -0,0 +1,13 @@
+package com.miliboy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiliBlogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiliBlogWebApplication.class, args);
+	}
+
+}
---
+++ mili-blog-web/src/test/java/com/miliboy/blog/MiliBlogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miliboy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiliBlogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
